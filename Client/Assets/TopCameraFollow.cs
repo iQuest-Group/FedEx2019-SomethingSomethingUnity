6efd81080bf629d8bb8ceeb17eed1a63d5bd60b2
mihnescu <mihnea.ilicevici@gmail.com>
2019-11-22 15:04:41 +0200

    Client - camera, collision, grid movement

---
+++ Client/Assets/TopCameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopCameraFollow : MonoBehaviour
+{
+
+    [SerializeField] private Transform Target;
+    [SerializeField] private float Speed = 1f;
+
+    private Vector3 offset;
+    private new Camera camera;
+
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+        if (Target != null)
+        {
+            offset = transform.position - Target.position;
+        }
+    }
+
+    void Update()
+    {
+        if (Target == null)
+        {
+            return;
+        }
+        var targetPos = Target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * Speed);
+    }
+}
