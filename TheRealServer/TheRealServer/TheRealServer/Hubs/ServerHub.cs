7564059334561d1b73c7ebe8f205bc2fb5e51f70
Alexandru Rotaru <alexandru.rotaru@iquestgroup.com>
2019-11-22 11:41:44 +0200

    Add SignalR server

---
+++ TheRealServer/TheRealServer/TheRealServer/Hubs/ServerHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace TheRealServer.Hubs
+{
+    public class ServerHub : Hub
+    {
+        public async Task SendMovement(float posX = 19, float posY = 20)
+        {
+            await Clients.All.SendAsync("ReceiveMovement", posX, posY);
+        }
+    }
+}
---
+++ TheRealServer/TheRealServer/TheRealServer/wwwroot/js/server.js
@@ -0,0 +1,20 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/server").build();
+
+connection.on("ReceiveMovement", function (posX, posY) {
+    document.getElementById("messagesList").value = posX + " "  + posY;
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    connection.invoke("SendMovement", 10, 1).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
